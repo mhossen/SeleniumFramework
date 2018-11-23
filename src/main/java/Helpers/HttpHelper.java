1abbf9b144a246248f938dcd8b742fa6bad66432
Mohammed Hossen <mhossen@wgu.edu>
2018-11-23 12:01:16 -0500

    added http status check

---
+++ src/main/java/Helpers/HttpHelper.java
@@ -0,0 +1,29 @@
+package Helpers;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+
+public class HttpHelper {
+
+    public String GetStatusCode(String url) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+          //   EntityUtils.consume(entity1);
+          return   entity1.toString();
+        } finally
+        {
+            response1.close();
+        }
+    }
+}
--- src/test/java/TestScripts/CheckUrlStatusTest.java
+++ src/test/java/TestScripts/CheckUrlStatusTest.java
@@ -2,11 +2,13 @@ package TestScripts;
 
 import Base.BaseDriver;
 import Helpers.FileHelper;
+import Helpers.HttpHelper;
 import Settings.ClassRepo;
 import Settings.ObjectRepo;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,17 +16,19 @@ public class CheckUrlStatusTest extends BaseDriver {
     String fileName = System.getProperty("user.dir") + "\\src\\main\\resources\\ListOfUrl.txt";
     List<String> urls = Collections.emptyList();
     FileHelper fileReader = new FileHelper(fileName);
+    HttpHelper httpHelper = new HttpHelper();
+
 
     @Test
-    public void StatusTest() {
+    public void StatusTest() throws IOException {
         urls = fileReader.GetAllLineAsList();
 
         for (String url : urls) {
             getClassRepo().get_navigation().NavigateToUrl(url);
-
-            if (!driver.getTitle().toLowerCase().contains("server information")) {
-                System.out.println(driver.getTitle());
-            }
+            System.out.println(httpHelper.GetStatusCode(url));
+//            if (!driver.getTitle().toLowerCase().contains("server information")) {
+//                System.out.println(driver.getTitle());
+//            }
         }
     }
 }
